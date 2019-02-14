dac0aa6d631ab4ac01ae1bbd4e0aa74919e37b2a
Kirk Wight <kwight@kwight.ca>
2019-02-14 12:17:36 -0800

    Add asetting to allow the JP site to be iframed by WordPress.com.

--- _inc/client/components/navigation-settings/index.jsx
+++ _inc/client/components/navigation-settings/index.jsx
@@ -141,6 +141,7 @@ export const NavigationSettings = createReactClass( {
 						'infinite-scroll',
 						'minileven',
 						'copy-post',
+						'wordpress-com-compose',
 					] ) && (
 						<NavItem
 							path="#writing"
